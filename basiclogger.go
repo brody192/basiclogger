20eb5a5f40797aa5addc658e86b81993be146814
Brody Over <brodyover@gmail.com>
2022-06-13 00:59:59 -0400

    inital commit

---
+++ basiclogger.go
@@ -0,0 +1,22 @@
+package basiclogger
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var BasicLogger = &basicLogger{logger: io.Writer(os.Stdout)}
+
+type basicLogger struct {
+	logger io.Writer
+}
+
+func (l *basicLogger) Error(err error) {
+	l.logger.Write([]byte(err.Error()))
+}
+
+func (l *basicLogger) Write(p []byte) (n int, err error) {
+	fmt.Fprintln(l.logger, string(p))
+	return len(p), nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/brody192/basiclogger
+
+go 1.18
