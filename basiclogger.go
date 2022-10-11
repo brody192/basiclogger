78b8afeaa2a9b9741415bc85c0e414c48490c1fa
overrnet <brodyover@gmail.com>
2022-10-11 01:50:55 -0400

    added log levels

--- basiclogger.go
+++ basiclogger.go
@@ -1,22 +1,41 @@
-package basiclogger
+package main
 
 import (
-	"fmt"
-	"io"
+	"log"
 	"os"
 )
 
-var Logger = &basicLogger{logger: io.Writer(os.Stdout)}
-
 type basicLogger struct {
-	logger io.Writer
+	// logs [INFO] without "log.Lshortfile"
+	InfoBasic *log.Logger
+	Info      *log.Logger
+	Warn      *log.Logger
+	Error     *log.Logger
 }
 
-func (l *basicLogger) Error(err error) {
-	l.logger.Write([]byte(err.Error()))
+type echoLogger struct {
+	logger basicLogger
 }
 
-func (l *basicLogger) Write(p []byte) (n int, err error) {
-	fmt.Fprintln(l.logger, string(p))
+var (
+	Logger     = &basicLogger{}
+	EchoLogger = &echoLogger{}
+)
+
+func init() {
+	Logger.Info = log.New(os.Stdout, "[INFO] ", log.Lshortfile)
+	Logger.InfoBasic = log.New(os.Stdout, "[INFO] ", 0)
+	Logger.Warn = log.New(os.Stdout, "[WARNING] ", log.Lshortfile)
+	Logger.Error = log.New(os.Stdout, "[ERROR] ", log.Lshortfile)
+
+	EchoLogger.logger = *Logger
+}
+
+func (l *echoLogger) Error(err error) {
+	l.logger.Error.Println(err)
+}
+
+func (l *echoLogger) Write(p []byte) (n int, err error) {
+	l.logger.InfoBasic.Println(string(p))
 	return len(p), nil
 }
