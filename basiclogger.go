b4747a9f51a1f9da6db9ce3cf96cb51541b069fe
overrnet <brodyover@gmail.com>
2022-10-27 02:59:56 -0400

    remove file and line from error printout, add check for duplicate prints

--- basiclogger.go
+++ basiclogger.go
@@ -6,10 +6,11 @@ import (
 )
 
 var (
-	InfoBasic *log.Logger
-	Info      *log.Logger
-	Warn      *log.Logger
-	Error     *log.Logger
+	InfoBasic  *log.Logger
+	Info       *log.Logger
+	Warn       *log.Logger
+	Error      *log.Logger
+	errorBasic *log.Logger
 
 	EchoLogger = &echoLogger{}
 )
@@ -21,10 +22,16 @@ func init() {
 	InfoBasic = log.New(os.Stdout, "[INFO] ", 0)
 	Warn = log.New(os.Stdout, "[WARNING] ", log.Lshortfile)
 	Error = log.New(os.Stdout, "[ERROR] ", log.Lshortfile)
+	errorBasic = log.New(os.Stdout, "[ERROR] ", 0)
 }
 
+var lastError error
+
 func (l *echoLogger) Error(err error) {
-	Error.Println(err)
+	if err != lastError {
+		lastError = err
+		errorBasic.Println(err)
+	}
 }
 
 func (l *echoLogger) Write(p []byte) (n int, err error) {
