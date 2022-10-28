94a5b638b9f952893742ecbcf892e55aa4214cf4
overrnet <brodyover@gmail.com>
2022-10-27 21:01:06 -0400

    add logging to file

--- basiclogger.go
+++ basiclogger.go
@@ -1,8 +1,12 @@
 package basiclogger
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os"
+	"time"
+	"unsafe"
 )
 
 var (
@@ -13,9 +17,20 @@ var (
 	errorBasic *log.Logger
 
 	EchoLogger = &echoLogger{}
+
+	tz *time.Location
 )
 
 type echoLogger struct{}
+
+type logWriter struct {
+	destination io.Writer
+}
+
+func (lw *logWriter) Write(b []byte) (int, error) {
+	fmt.Fprintf(lw.destination, "%s %s", time.Now().In(tz).Format("Jan-01-Mon-02-03:04.05-PM-MST"), *(*string)(unsafe.Pointer(&b)))
+	return len(b), nil
+}
 
 func init() {
 	Info = log.New(os.Stdout, "[INFO] ", log.Lshortfile)
@@ -34,7 +49,41 @@ func (l *echoLogger) Error(err error) {
 	}
 }
 
-func (l *echoLogger) Write(p []byte) (n int, err error) {
-	InfoBasic.Println(string(p))
-	return len(p), nil
+func (l *echoLogger) Write(b []byte) (n int, err error) {
+	InfoBasic.Println(*(*string)(unsafe.Pointer(&b)))
+	return len(b), nil
+}
+
+var fileOpen = false
+
+func SaveLogFile(filename string) {
+	if filename == "" {
+		panic("need filename")
+	}
+
+	if !fileOpen {
+		fileOpen = true
+
+		LoadLocation("America/Toronto")
+
+		log, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600)
+		if err != nil {
+			panic(err)
+		}
+
+		var multi = io.MultiWriter(os.Stdout, &logWriter{destination: log})
+		Info.SetOutput(multi)
+		InfoBasic.SetOutput(multi)
+		Warn.SetOutput(multi)
+		Error.SetOutput(multi)
+		errorBasic.SetOutput(multi)
+	}
+}
+
+func LoadLocation(location string) {
+	l, err := time.LoadLocation(location)
+	if err != nil {
+		panic(err)
+	}
+	tz = l
 }
