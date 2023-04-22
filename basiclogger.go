9d8953eb4b4cc17b69ec8916d4b6a5f47202244f
Brody Over <brodyover@gmail.com>
2023-04-22 14:41:07 -0400

    clean

--- basiclogger.go
+++ basiclogger.go
@@ -1,46 +1,14 @@
 package basiclogger
 
 import (
-	"bytes"
-	"errors"
 	"log"
 	"os"
-	"strings"
-	"unsafe"
 )
 
 var (
-	InfoBasic  *log.Logger
-	Info       *log.Logger
-	Warn       *log.Logger
-	Error      *log.Logger
-	errorBasic *log.Logger
-
-	EchoLogger = &echoLogger{}
+	Info       = log.New(os.Stdout, "[INFO] ", log.Lshortfile)
+	InfoBasic  = log.New(os.Stdout, "[INFO] ", 0)
+	Warn       = log.New(os.Stdout, "[WARNING] ", log.Lshortfile)
+	Error      = log.New(os.Stderr, "[ERROR] ", log.Lshortfile)
+	ErrorBasic = log.New(os.Stderr, "[ERROR] ", 0)
 )
-
-type echoLogger struct{}
-
-func init() {
-	Info = log.New(os.Stdout, "[INFO] ", log.Lshortfile)
-	InfoBasic = log.New(os.Stdout, "[INFO] ", 0)
-	Warn = log.New(os.Stdout, "[WARNING] ", log.Lshortfile)
-	Error = log.New(os.Stdout, "[ERROR] ", log.Lshortfile)
-	errorBasic = log.New(os.Stdout, "[ERROR] ", 0)
-}
-
-var lastError error
-
-func (l *echoLogger) Error(err error) {
-	err = errors.New(strings.TrimSpace(err.Error()))
-	if err != lastError {
-		lastError = err
-		errorBasic.Println(err)
-	}
-}
-
-func (l *echoLogger) Write(b []byte) (n int, err error) {
-	b = bytes.TrimSpace(b)
-	InfoBasic.Println(*(*string)(unsafe.Pointer(&b)))
-	return len(b), nil
-}
